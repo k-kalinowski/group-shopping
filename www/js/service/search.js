d3c1225b585fea78c0493ed3bc17b938807a6b35
Łukasz Olejarczuk <ol3j@o2.pl>
2015-03-13 23:04:41 +0100

    production url

--- www/js/service/search.js
+++ www/js/service/search.js
@@ -4,9 +4,10 @@ angular.module('starter.services')
     .factory('searchService', function ($http) {
         return {
             getItems: function(text) {
-                return $http.post('api/offers?access_token=07de65799b3bd1e90f3004e33086f23c',
-                '{"limit":25,"searchString":'+ text + ',"category":165,"sort":"price:desc" }');
+                    return $http.post('https://api.natelefon.pl:9901/v2/allegro/offers?access_token=07de65799b3bd1e90f3004e33086f23c',
+                    '{"limit":25,"searchString":'+ text + ',"category":165,"sort":"price:desc" }');
+                //return $http.post('api/offers?access_token=07de65799b3bd1e90f3004e33086f23c',
+                //'{"limit":25,"searchString":'+ text + ',"category":165,"sort":"price:desc" }');
             }
         }
-
     });
