36008edeec1f222fb02d9d7ea8dc03e6c5653af6
Kamil Kalinowski <kalinowski.kml@gmail.com>
2015-03-14 16:12:21 +0100

    sdaf

--- www/js/service/service.js
+++ www/js/service/service.js
@@ -2,6 +2,6 @@
  * Created by lukaszolejarczuk on 13/03/15.
  */
 angular.module('starter.services', [])
-    //.value('restUrl', 'https://api.natelefon.pl:9901/v2/allegro')
-    .value('restUrl', '/api')
-    .value('accessToken', '39bbc694f119b7241177dc3a08e31c2b');
+    .value('restUrl', 'https://api.natelefon.pl:9901/v2/allegro')
+//    .value('restUrl', '/api')
+    .value('accessToken', '0a67a01f2c3ae3e23bd2bcb485997cb6');
